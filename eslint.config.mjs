2260e3c3c2aab7ac0ed3150d0222e0e0ac18c613
arnite <udembao429@gmail.com>
2025-11-18 12:24:51 +0100

    initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+import tseslint from 'typescript-eslint';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+
+export default tseslint.config(...tseslint.configs.recommended, {
+  plugins: {
+    prettier: eslintPluginPrettier,
+  },
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        semi: true,
+        trailingComma: 'es5',
+      },
+    ],
+    'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+  },
+});
